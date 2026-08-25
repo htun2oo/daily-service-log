6b22bff89b70abb296675e80773d6362a2184ef7
htun2oo <htun2oo@gmail.com>
2026-08-25 23:27:45 +0630

    Change MyApp from StatelessWidget to StatefulWidget
    Refactor MyApp to use StatefulWidget for theme management.

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,44 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  void changeTheme(ThemeMode themeMode) {
+    setState(() {
+      _themeMode = themeMode;
+    });
+  }
+
+  ThemeMode get currentTheme => _themeMode;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Daily Service Log',
       debugShowCheckedModeBanner: false,
+      themeMode: _themeMode,
       theme: ThemeData(
+        brightness: Brightness.light,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          brightness: Brightness.dark,
+        ),
         useMaterial3: true,
       ),
       home: const LogListScreen(),
